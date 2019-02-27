0a037ed0d09b9d06b2dcb57c6069ae623b0a6a74
Johanna Liebler <johanna.liebler@gmail.com>
2019-02-27 19:55:44 +0100

    Challenge

--- hello.py
+++ hello.py
@@ -1,2 +1,10 @@
 name = input("What is your Name? ")
-print( "Hello " + name + ", nice to meet you!")
+
+if name == "Felix":
+    print("Hello Felix, wahhhhhhh")
+elif name == "Dominik":
+    print("Hello Dominik, i love you. You are amazing")
+elif name == "Johanna":
+    print("Hello Johanna, how are you?")
+else:
+    print( "Hello " + name + ", nice to meet you!")
