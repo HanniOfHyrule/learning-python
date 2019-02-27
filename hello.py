617ab8697058e715fb0fe33cd2d0268efd7e6425
Johanna Liebler <johanna.liebler@gmail.com>
2019-02-27 20:15:28 +0100

    Challenge2

--- hello.py
+++ hello.py
@@ -1,10 +1,13 @@
-name = input("What is your Name? ")
+def find_greeting(name):
+    if name == "Felix":
+        print("Hello Felix, wahhhhhhh")
+    elif name == "Dominik":
+        print("Hello Dominik, i love you. You are amazing")
+    elif name == "Johanna":
+        print("Hello Johanna, how are you?")
+    else:
+        print( "Hello " + name + ", nice to meet you!")
 
-if name == "Felix":
-    print("Hello Felix, wahhhhhhh")
-elif name == "Dominik":
-    print("Hello Dominik, i love you. You are amazing")
-elif name == "Johanna":
-    print("Hello Johanna, how are you?")
-else:
-    print( "Hello " + name + ", nice to meet you!")
+while True:
+    myname = input("What is your Name? ")
+    find_greeting(myname)
