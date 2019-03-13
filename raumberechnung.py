58f8352a5fc23fc61edecf2e695a27ddbea7bf66
Johanna Liebler <johanna.liebler@gmail.com>
2019-03-13 19:23:16 +0100

    caesar finish

---
+++ raumberechnung.py
@@ -0,0 +1,7 @@
+print "What is the lenght of the room?"
+length = input()
+print "What is the width of the room?"
+width = input()
+
+size = length * width
+print "The size of the room is: %f" % (size)
