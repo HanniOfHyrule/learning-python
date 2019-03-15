e7d252653a2e517a1271238236d54aecb7d4bdf5
Johanna Liebler <johanna.liebler@gmail.com>
2019-03-15 20:32:56 +0100

    fibonacci

--- schildi.py
+++ schildi.py
@@ -2,13 +2,18 @@ import turtle
 
 window = turtle.Screen()
 turtle.shape("turtle")
-size = 1
-turtle.pensize(0.4)
+size = 0
+
+def fibonacci(index):
+    if index == 0 or index == 1:
+        return 1
+    else:
+        return fibonacci(index - 2)+ fibonacci(index-1)
 
 while True:
     for x in range(0,4):
         turtle.speed(x + 50)
-        turtle.forward(size)
+        turtle.forward(fibonacci(size))
         turtle.right(90)
     turtle.left(5)
     size += 1
