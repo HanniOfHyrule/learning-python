eb11c1e1cd24c9ee297ef35a6d08b218bddf565b
Johanna Liebler <johanna.liebler@gmail.com>
2019-03-15 20:22:06 +0100

    von klein mach groß

--- schildi.py
+++ schildi.py
@@ -2,13 +2,15 @@ import turtle
 
 window = turtle.Screen()
 turtle.shape("turtle")
+size = 1
+turtle.pensize(0.4)
 
 while True:
     for x in range(0,4):
         turtle.speed(x + 50)
-        turtle.forward(100)
+        turtle.forward(size)
         turtle.right(90)
     turtle.left(5)
-
+    size += 1
 
 window.mainloop()
